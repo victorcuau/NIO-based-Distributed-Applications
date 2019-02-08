1655879e4a1cffe02fd886bbcac74370e2bb0249
Victor CUAU <victor.cuau@hotmail.fr>
2019-02-08 11:17:02 +0100

    SecondBabyStep : create Reader

---
+++ ricm4-tp-nio-projects-DONNE/ricm.nio.babysteps/babysteps/ricm/distsys/nio/babystep2/Reader.java
@@ -0,0 +1,19 @@
+package ricm.distsys.nio.babystep2;
+
+public class Reader {
+	
+	enum State {IDLE};
+	
+	Reader(){
+		
+	}
+	
+	
+	public void handleRead() {
+		
+	}
+	
+	
+	//public void processMsg(byte[]);
+	
+}
