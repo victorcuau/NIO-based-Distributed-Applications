779c7147d2a521fe42b4bb3605a660aeeafabdf4
Leia <leya.badat@live.fr>
2019-02-22 10:02:48 +0100

    FirstTask : création du projet + classes Channel et ChannelListener

---
+++ ricm4-tp-nio-projects-DONNE/ricm.channels.nio/src/ricm/channels/nio/Channel.java
@@ -0,0 +1,28 @@
+package ricm.channels.nio;
+
+import ricm.channels.IChannel;
+import ricm.channels.IChannelListener;
+
+public class Channel implements IChannel {
+
+	public void setListener(IChannelListener l) {
+		
+	}
+
+	public void send(byte[] bytes, int offset, int count) {
+		
+	}
+
+	public void send(byte[] bytes) {
+		
+	}
+
+	public void close() {
+		
+	}
+
+	public boolean closed() {
+		return false;
+	}
+
+}
---
+++ ricm4-tp-nio-projects-DONNE/ricm.channels.nio/src/ricm/channels/nio/ChannelListener.java
@@ -0,0 +1,19 @@
+package ricm.channels.nio;
+import ricm.channels.IChannel;
+import ricm.channels.IChannelListener;
+
+public class ChannelListener implements IChannelListener {
+
+	@Override
+	public void received(IChannel c, byte[] bytes) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void closed(IChannel c, Exception e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
